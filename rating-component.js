bc80e50a260497c80baa360fc841a6ddd9d6792c
Jorge <jorge.cast.cont@gmail.com>
2019-11-24 17:16:29 -0600

    rating component

---
+++ rating-component.js
@@ -0,0 +1,54 @@
+import {html, LitElement} from "lit-element";
+import '@polymer/iron-icon';
+import '@polymer/iron-icons/iron-icons';
+import style from './rating-component-styles';
+
+class RatingComponent extends LitElement {
+    constructor() {
+        super();
+    }
+
+    static get properties() {
+        return {
+            rate: {
+                type: Number,
+                value: 2
+            }
+        };
+    }
+
+    static get styles() {
+        return style;
+    }
+
+    updated(_changedProperties) {
+        if(_changedProperties.has('rate')){
+            const stars = this.shadowRoot.querySelectorAll('.star');
+            this.__clearStar(stars);
+            for (let index = 0; index < this.rate; index++) {
+                const star = stars[index];
+                star.classList.add('active');
+            }
+        }
+    }
+
+    __clearStar(stars) {
+        for (const star of stars) {
+            star.classList.remove('active');
+        }
+    }
+
+    render() {
+        return html`
+            <div class="rating-container">
+                <iron-icon class="star" icon="star"></iron-icon>
+                <iron-icon class="star" icon="star"></iron-icon>
+                <iron-icon class="star" icon="star"></iron-icon>
+                <iron-icon class="star" icon="star"></iron-icon>
+                <iron-icon class="star" icon="star"></iron-icon>
+            </div>
+        `;
+    }
+}
+
+window.customElements.define('rating-component', RatingComponent);
